6b4af0e3f08597230b27df1574766ea03b13fd5e
M-Komorek <marcin.komorek@protonmail.com>
2025-01-25 18:59:17 +0100

    lazyvim base config

---
+++ lua/plugins/lsp.lua
@@ -0,0 +1,18 @@
+return {
+  {
+    "neovim/nvim-lspconfig",
+    opts = {
+      servers = {
+        clangd = {},
+        dockerls = {},
+        gopls = {},
+        html = {},
+        jsonls = {},
+        lua_ls = {},
+        lemminx = {},
+        pyright = {},
+        rust_analyzer = {},
+      },
+    },
+  },
+}
---
+++ lua/plugins/ts.lua
@@ -0,0 +1,27 @@
+return {
+  {
+    "nvim-treesitter/nvim-treesitter",
+    opts = {
+      ensure_installed = {
+        "bash",
+        "cpp",
+        "html",
+        "go",
+        "javascript",
+        "json",
+        "lua",
+        "make",
+        "markdown",
+        "markdown_inline",
+        "python",
+        "query",
+        "regex",
+        "rust",
+        "tsx",
+        "typescript",
+        "vim",
+        "yaml",
+      },
+    },
+  },
+}
